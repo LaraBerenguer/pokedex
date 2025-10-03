691b4f22d5a0a6434657b42da6d8f3eec5ac8f61
LaraBerenguer <laraberenguer@outlook.com>
2025-10-03 22:16:16 +0200

    Update title in index.html to include accent and wrap BrowserRouter with PokemonProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import AppRoutes from './routes/AppRoutes.tsx'
+import { PokemonProvider } from './context/PokemonContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <PokemonProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </PokemonProvider>
   </StrictMode>
 )
