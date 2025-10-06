6e9a697028dd094519970b3d691041042fe1acfe
LaraBerenguer <laraberenguer@outlook.com>
2025-10-07 00:12:13 +0200

    Add type list to Home.tsx component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import PokemonCard from "../components/PokemonCard";
 import Search from "../components/Search";
+import Badge from "../components/ui/Badge";
 import Spinner from "../components/ui/Spinner";
 import { usePokemonContext } from "../context/PokemonContext";
 
 function Home() {
-  const { filteredPokemons, loading, error } = usePokemonContext()
-  if (loading) { return <Spinner /> }
-  if (error) {
+  const { filteredPokemons, pokemonLoading, pokemonError, types, typesLoading, typesError } = usePokemonContext()
+  if (pokemonLoading) { return <Spinner /> }
+  if (pokemonError) {
     return (
       <div className="text-center p-8">
-        <p className="text-red-500 mb-4">{error}</p>
+        <p className="text-red-500 mb-4">{pokemonError}</p>
         <p className="text-gray-600 text-sm">Try refreshing the page</p>
         <button className="text-sm text-decoration-line" onClick={() => window.location.reload()}>Refresh</button>
       </div>
@@ -21,10 +22,21 @@ function Home() {
       <section className="home-search">
         <Search />
       </section>
-      <section className="home-filter">{/*Filter type and search section*/}</section>
+      <section className="home-filter">
+        {typesLoading ?
+          (<p className="text-xs w-full text-center">Loading types...</p>
+          ) : typesError ? (
+            (<p className="text-sm w-full text-center">Error loading types: {typesError}</p>)
+          ) : (
+            types.map(type => (
+              <Badge type={type} />
+            ))
+          )
+        }
+      </section>
       <section className="home-list gap-2 flex flex-wrap">
         {filteredPokemons.length ? filteredPokemons.map(p => <PokemonCard key={p.id} pkm={p} />) :
-            <p className="text-sm w-full text-center">Oops! There's no pokemon in this tall grass 🍃</p>}
+          <p className="text-sm w-full text-center">Oops! There's no pokemon in this tall grass 🍃</p>}
       </section>
     </section>
   );
