c759b3a7300c40b78b89f3a0f69bb612dbad1f64
LaraBerenguer <laraberenguer@outlook.com>
2025-10-07 22:15:48 +0200

    Add go back button in Details and implemented it

--- src/components/DetailsCard.tsx
+++ src/components/DetailsCard.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router";
 import type { Pokemon } from "../types/types";
 import { capitalizeFirstLetter } from "../utils/capitalize";
+import Stats from "./Stats";
 import Badge from "./ui/Badge";
+import GoBackButton from "./ui/GoBackButton";
 
 interface DetailsCardProps {
     pkm: Pokemon | undefined;
 }
 
 const DetailsCard = ({ pkm }: DetailsCardProps) => {
-    console.log(pkm)
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate("/")
+    };
+
     if (pkm === undefined) {
         return <p className="text-sm w-full text-center">Oops! Your pokemon is not here 🍃</p>
     }
@@ -15,8 +23,8 @@ const DetailsCard = ({ pkm }: DetailsCardProps) => {
 
     return (
         <div className="details-card bg-white border border-gray-200 shadow-md rounded-2xl p-6 flex flex-col items-center gap-4 max-w-sm w-full text-center">
-            <div className="details-card--back">
-                {/*button go back*/}
+            <div className="details-card--back self-start">
+                <GoBackButton onClick={handleGoBack} />
             </div>
             <div className="details-card--info">
                 <section className="details-card--name text-2xl font-semibold text-gray-800">
@@ -29,18 +37,7 @@ const DetailsCard = ({ pkm }: DetailsCardProps) => {
                     {pkm.types.map(t => <Badge key={t.name} type={t.name} />)}
                 </section>
                 <div className="details-card--stats w-full mt-4">
-                    <h3 className="text-lg font-medium text-gray-700 mb-2">Stats</h3>
-                    <section className="grid grid-cols-2 gap-y-2 text-sm text-gray-600">
-                        {pkm.stats.map((s) => (
-                            <div
-                                key={s.name}
-                                className="flex justify-between px-2 py-1 bg-gray-50 rounded-md"
-                            >
-                                <span className="capitalize">{s.name}</span>
-                                <span className="font-semibold text-gray-800">{s.base}</span>
-                            </div>
-                        ))}
-                    </section>
+                    <Stats pkm={pkm} />
                 </div>
             </div>
         </div>
