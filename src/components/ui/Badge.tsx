2753b806a6a513d3564e591b16bc8847bf8089f4
LaraBerenguer <laraberenguer@outlook.com>
2025-10-08 19:19:53 +0200

    Add border to type filter when selected

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -2,12 +2,14 @@ import { typeColors } from "../../utils/typeColors";
 
 interface TypeButtonProps {
     onClick?: () => void;
+    selected?: boolean;
     type: string;
 }
 
-const TypeButton = ({ onClick, type }: TypeButtonProps) => {
+const TypeButton = ({ onClick, selected = false, type }: TypeButtonProps) => {
     const color = typeColors[type.toLowerCase()] || "#A8A77A";
-    return <span className="text-sm text-white px-2 py-1 m-1 items-center rounded-lg hover:shadow-sm" style={{ backgroundColor: color, display: "inline-block", verticalAlign: "middle" }} onClick={onClick}>{type}</span>
+    const borderStyle = selected ? "3px solid #f472b6" : "3px solid transparent";
+    return <span className="text-sm text-white px-2 py-1 m-1 items-center rounded-lg hover:shadow-sm" style={{ backgroundColor: color, border: borderStyle, display: "inline-block", verticalAlign: "middle" }} onClick={onClick}>{type}</span>
 };
 
 export default TypeButton;
