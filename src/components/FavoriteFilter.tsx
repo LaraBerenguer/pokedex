c9d1c545bbfc74ea3824e6a88af06b1d61da9c25
LaraBerenguer <laraberenguer@outlook.com>
2025-10-08 14:04:22 +0200

    Add onclick to FavoriteFilter

--- src/components/FavoriteFilter.tsx
+++ src/components/FavoriteFilter.tsx
@@ -1,18 +1,22 @@
+import { usePokemonContext } from "../context/PokemonContext";
 
 
 const FavoriteFilter = () => {
+    const { setSelectedFilter } = usePokemonContext();
 
     return (
         <div className="favorite-filter justify-center flex gap-1">
             <button
                 type="button"
                 className="px-3 py-1 rounded-md bg-transparent hover:bg-gray-100 dark:hover:bg-gray-600 focus:bg-gray-100 dark:focus:bg-gray-600 text-sm font-medium transition act"
+                onClick={() => setSelectedFilter("all")}
             >
                 All
             </button>
             <button
                 type="button"
                 className="px-3 py-1 rounded-md bg-transparent hover:bg-gray-100 dark:hover:bg-gray-600 focus:bg-gray-100 dark:focus:bg-gray-600 text-sm font-medium transition"
+                onClick={() => setSelectedFilter("favorites")}
             >
                 Favorites
             </button>
